b3414090e0ef8e4059cce589b2db011cfc71f417
Vertigosoftware <sominascott7@gmail.com>
2026-06-29 16:58:53 +0100

    Change slider threshold from 80 to 50

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ if st.session_state.page == 'home':
     st.write(" ")
     slide = st.slider("slide right to continue", min_value=0, max_value=100, value=0)
     
-    if slide >= 80:
+    if slide >= 50:
         st.session_state.page = 'main'
         st.rerun()
 
